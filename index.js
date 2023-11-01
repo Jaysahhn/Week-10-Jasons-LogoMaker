6c7a0b4feada2f23144ddba95fb8f729104932a3
Jaysahhn <jasonmason96@yahoo.com>
2023-10-31 22:26:25 -0500

    added lib with shapes and shapes test. added inquirer, fs, and shapes

---
+++ index.js
@@ -0,0 +1,23 @@
+const { default: inquirer } = require("inquirer"); // take a look at later
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+const fs = require("fs");
+
+function init() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
